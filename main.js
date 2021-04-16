ce3513356e6646e5ab321d1175dd32f0c0f33bc9
LOLZ-guys <76886406+LOLZ-guys@users.noreply.github.com>
2021-04-16 19:34:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function (event) {
+    console.log(event);
+    var Content = event.result[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+
+    if (Content == "take my selfie") {
+        console.log("Taking my selfie --- ");
+        speak();
+    }
+
+}
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your Selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
+}
